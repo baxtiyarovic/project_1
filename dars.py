4f6786ce437733d28fb5f46bccdabad5add488f9
baxtiyarovic <169052977+baxtiyarovic@users.noreply.github.com>
2024-05-06 17:34:26 +0500

    Add files via upload

---
+++ dars.py
@@ -0,0 +1,72 @@
+
+
+
+
+# name = "Aziz"
+# job = "Back End Developer"
+# year = 2004
+# age = 2024 - year
+# print(name)
+# print(job)
+# print(year)
+# print(age)
+#
+
+
+
+
+#Dada = 1981
+#Men = 2004
+#age = Dada - Men
+#print(age)
+
+
+# # yili = 2004
+# # Yoshi = 2024 - 2004
+# # Oyi = 100
+# # Moshina = 100000
+# # Natija = Moshina / Oyi
+# # print(ism)
+# # print(Yoshi)
+# # print(Natija)
+# ism = "Aziz
+
+
+# a = "2323"
+# print(type(a))
+# name = "Aziz"
+# a = "@gmail.com"
+# email = name + a
+# print(email)
+
+# a = 12
+# b = 12
+# c = a + b
+# print(c)
+#
+# a = 98
+# b = 12
+# c = a + b
+# print(c)
+
+
+# a = 12.2
+# b = 26
+# c = a + b
+# print(c)
+#
+# a = 123.7
+# b = 256.9
+# c = a + b
+# print(c)
+
+a = 12
+b = 13
+c = a + b
+print(type(c), c)
+
+
+
+
+
+
